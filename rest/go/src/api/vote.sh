02b0e12693dee4abefad3c3bfc4d204ec54ab94c
Rafael Feijo da Rosa <raphael.exe@gmail.com>
2017-04-26 10:38:17 -0300

    CHANGE - Alterado o id da alternativa para 1

--- rest/go/src/api/vote.sh
+++ rest/go/src/api/vote.sh
@@ -1,1 +1,1 @@
-curl -c cookies.txt -b cookies.txt -H "Content-Type: application/json" -X POST -d '{"alternative_id":4}' http://localhost/rest/vote/1
+curl -c cookies.txt -b cookies.txt -H "Content-Type: application/json" -X POST -d '{"alternative_id":1}' http://localhost/rest/vote/1
